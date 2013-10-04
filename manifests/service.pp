dce1ba5d71d93e08ed88751ad099c34fddfc96bb
Yunxao <francisco.huertas@centeropenmiddleware.com>
2013-10-04 16:57:14 +0200

    Service now work but using exec, updating the template of the service

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,26 @@
 class tomcat7::service() {
+  include tomcat7::params
   $tomcat_service_action = "${tomcat7::params::tomcat_service_action}"
   $tomcat_service_name   = "${tomcat7::params::tomcat_service_name}"
+  $service_path          = "${tomcat7::params::service_path}"
+#  notify {"---${tomcat_service_name} ${tomcat_service_action}---" : }
+#   file {"${service_path}/${tomcat_service_name}" : 
+#      path     => "${service_path}/${tomcat_service_name}",
+#      notify   => Service['tomcat7'], 
+#  }
+#  service {"tomcat7" :
+#    binary => "${service_path}/${tomcat_service_name}",
+#    enable => true, 
+#    ensure => "running", 
+#    start  => ' start', 
+#  }
 
-  service {"${tomcat_service_name}" :
-    name   => "${tomcat_service_name}",
-    ensure => true
+  exec {"service_start" : 
+    cwd     => "${service_path}", 
+    command => "${service_path}/${tomcat_service_name} ${tomcat_service_action}", 
   }
+#  service {"${tomcat_service_name}" :
+#    name   => "${tomcat_service_name}",
+#    ensure => running,
+#  }
 }
